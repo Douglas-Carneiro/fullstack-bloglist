6eb242a82ca4cdb86c7cf085a617f26753323b62
Douglas-Carneiro <douglasdscplay@gmail.com>
2021-12-02 16:09:22 -0300

    update url in commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,17 +26,17 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('login', ({ username, password }) => {
-  cy.request('POST', 'http://localhost:3001/api/login', {
+  cy.request('POST', 'http://localhost:8000/api/login', {
     username, password
   }).then(({ body }) => {
     localStorage.setItem('loggedNoteappUser', JSON.stringify(body))
-    cy.visit('http://localhost:3001')
+    cy.visit('http://localhost:8000')
   })
 })
 
 Cypress.Commands.add('createBlog', ({ title, author, url, likes }) => {
   cy.request({
-    url: 'http://localhost:3001/api/blogs',
+    url: 'http://localhost:8000/api/blogs',
     method: 'POST',
     body: { title, author, url, likes },
     headers: {
@@ -44,5 +44,5 @@ Cypress.Commands.add('createBlog', ({ title, author, url, likes }) => {
     }
   })
 
-  cy.visit('http://localhost:3001')
+  cy.visit('http://localhost:8000')
 })
